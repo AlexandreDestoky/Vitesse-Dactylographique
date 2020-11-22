ecd5fc88f9fad7a3f05688e9ba1943caf63d88f8
Destoky <adestoky@gmail.com>
2020-11-22 19:32:59 +0100

    Fin
    Projet terminer, rajout d'un bouton de commencement et amélioration de l'aspect esthétique

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 //SELECTION DU DOM
 const text = document.querySelector(".text");
 const btnRejouer = document.querySelector(".rejouer");
+const score = document.querySelector(".score");
 const erreur = document.querySelector(".erreur");
 const timer = document.querySelector(".timer");
+const annonce = document.querySelector(".annonce");
+const btnStart = document.querySelector(".start");
 
 //LISTE DE PHRASES
-let phrases = ["tomate", "sanglier", "biere"];
+let phrases = [
+  "Bien protégé à l'ouest par une colline culminant à l'altitude de 475 m, cette localité ardennaise se trouve à la naissance d'un vallon.",
+  "Le village est traversé par la route nationale 826 Houffalize-Libramont. Il se situe aux confins des communes de Houffalize (hameau de Vellereux) et de Bastogne (village de Vaux). Le village se prolonge au nord par le hameau de Rastate et au sud par celui de Vivrou.",
+  "Implantée au sommet d'une petite butte, l'église Saint Martin batie en 1949 dans une architecture contemporaine par l'architecte Léon Lamy d'Arlon a la particularité d'avoir été intégrée à un groupe de maisons et fermes anciennes. La tour du clocher datant de 1897 a été rehaussée en 1959.",
+  "L'ermitage de Saint-Gossé et le chemin des Morts se trouvent dans les bois en direction de Recogne. L'ermitage fait toujours l'objet d'un pèlerinage en mémoire de Gossé, un ermite qui vécut à la fin du XVIIe siècle et au début du XVIIIe siècle. Il n'a jamais été béatifié ni canonisé",
+  "Compogne possède une école communale. Le club de football du Royal Football Club de Compogne-Bertogne se trouve au hameau de Vivrou. Plusieurs gites ruraux se trouvent dans le village."
+];
 
 //VARIABLES DE TRAVAIL
 let phraseAffiche, tabLettres;
@@ -14,7 +23,6 @@ let finiTest, caracteres;
 let nbrRand, nbrRandomPartieAvant;
 let nbrErreur = 0;
 let chrono;
-
 
 //GENERE UNE PHRASE ALEATOIRE ET L'AFFICHE
 let phraseRandom = () => {
@@ -37,20 +45,20 @@ let phraseRandom = () => {
 let lancement = () => {
   caracteres = document.querySelectorAll(".text span");
   finiTest = false;
-
+  score.style.border = "";
   //Style par défaut avant même de taper sur une touche
-  caracteres[cpt].style.background = "green";
+  caracteres[cpt].style.background = "navy";
   caracteres[cpt].style.color = "white";
   caracteres[cpt].style.padding = "3px";
+  erreur.innerHTML = `${nbrErreur} Erreur`;
   temps();
 };
-
 
 let temps = () => {
   let m = 0;
   let s = 0;
   timer.innerHTML = `0${m}:0${s}`;
-   chrono = setInterval(() => {
+  chrono = setInterval(() => {
     s++;
     s > 9 ? (timer.innerHTML = `0${m}:${s}`) : (timer.innerHTML = `0${m}:0${s}`);
     if (s > 59) {
@@ -60,10 +68,12 @@ let temps = () => {
   }, 1000);
 };
 
-phraseRandom();
-lancement();
-
-erreur.innerHTML = `${nbrErreur} Erreur`;
+//Lancement a l'aide du bouton 
+btnStart.addEventListener("click", () => {
+  annonce.style.display = "none";
+  phraseRandom();
+  lancement();
+});
 
 //DETECTION DES TOUCHES
 document.body.addEventListener("keyup", (e) => {
@@ -76,27 +86,28 @@ document.body.addEventListener("keyup", (e) => {
     if (e.key == text.textContent[cpt]) {
       caracteres[cpt].style.padding = "0px";
       caracteres[cpt].style.background = "white";
-      caracteres[cpt].style.color = "green";
+      caracteres[cpt].style.color = "navy";
 
       if (cpt + 1 < caracteres.length) {
         cpt++;
-        caracteres[cpt].style.background = "green";
+        caracteres[cpt].style.background = "navy";
         caracteres[cpt].style.color = "white";
         caracteres[cpt].style.padding = "3px";
       } else {
-        console.log("sortie");
         finiTest = true;
-        btnRejouer.style.display = "block";
+        setTimeout(() => {
+          btnRejouer.style.display = "block";
+        }, 500);
         clearInterval(chrono);
+        score.style.border = "10px solid navy";
       }
     } else {
       caracteres[cpt].style.background = "red";
       nbrErreur++;
       erreur.innerHTML = `${nbrErreur} Erreur`;
-      nbrErreur>1?erreur.innerHTML+="s":"";
+      nbrErreur > 1 ? (erreur.innerHTML += "s") : "";
     }
   }
-  console.log(cpt + " " + caracteres.length);
 });
 
 //DETECTION CLICK BTN REJOUER
